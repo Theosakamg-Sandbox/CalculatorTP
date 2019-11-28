49b6bc7794f32ffc768b927f9ecf3469e22fc10a
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2019-11-28 12:00:38 +0100

    Initial Commit

---
+++ CalculatorUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5335fe5e-9812-4537-b27a-0f8271d7f108")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Console/CalculatorEngine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Console
+{
+    public class CalculatorEngine
+    {
+        /// <summary>
+        /// Function to Addition two value.
+        /// </summary>
+        /// <param name="val1">1st value.</param>
+        /// <param name="val2">2nd value.</param>
+        /// <returns>the addition value.</returns>
+        public int Add(int val1, int val2)
+        {
+            return val1 + val2;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val1"></param>
+        /// <param name="val2"></param>
+        /// <returns></returns>
+        public int Sub(int val1, int val2)
+        {
+            return val1 - val2;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val1"></param>
+        /// <param name="val2"></param>
+        /// <returns></returns>
+        public int Mul(int val1, int val2)
+        {
+            return val1 * val2;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val1"></param>
+        /// <param name="val2"></param>
+        /// <returns></returns>
+        public int Div(int val1, int val2)
+        {
+            int res;
+
+            if (val2 != 0)
+            {
+                res = val1 / val2;
+            }
+            else
+            {
+                res = Int32.MinValue;
+            }
+
+            return res;
+        }
+    }
+}
